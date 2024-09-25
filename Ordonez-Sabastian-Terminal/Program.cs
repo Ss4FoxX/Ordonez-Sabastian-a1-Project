acb365662c7b1f733b9c65f19464688326ba72c1
Sabastian ordonez <Ss4FoxX@outlook.com>
2024-09-25 18:54:33 -0400

    Basic Code

--- Ordonez-Sabastian-Terminal/Program.cs
+++ Ordonez-Sabastian-Terminal/Program.cs
@@ -4,7 +4,7 @@ int Gold = 0;
 
 
 Console.WriteLine("Hello, I  am Irrelivance,I will be your narrirator. Let us begin, You awaken at a crossroads, all but a bag of 50 Gold, where will you go?");
-Console.WriteLine("1, to the right a Shaded Forest, 2, to the left are vast plains, finally 3, behind you to a dense jungle. where do you go? ");
+Console.WriteLine("1, to the right a Shaded Forest, 2, to the left are vast plains");
 
 Gold += 50;
 string Choice = Console.ReadLine();
@@ -16,7 +16,7 @@ if (Choice == "1")
     Console.WriteLine("you have 2 Choices, either 1, sneak past it, or try to attack it. your choice.");
     Choice = Console.ReadLine();
     if (Choice == "1")
-    { 
+    {
         Console.WriteLine("You manage to sneak past, you see the Juvenile Golblin over a clearly poisoned rabbit. While you continue along your treck, you notice something intesting. just off the path.");
         Console.WriteLine("A graveyard with graves made of stones and wood, you notice many riches, items and weapons around the graves.");
         Console.WriteLine("You can respect the dead, or just ignore the graves and riches. You may steal all the riches of the grave blind aswell, Choose.");
@@ -41,10 +41,6 @@ if (Choice == "1")
     }
 }
 else if (Choice == "2")
-{
-    
-}
-else if (Choice == "3")
 {
 
 }
